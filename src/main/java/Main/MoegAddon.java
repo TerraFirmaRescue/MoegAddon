5936ad32b9bdd693420a2601033a234b4053914f
yuesha-yc <kevinwangyichen@gmail.com>
2020-03-16 11:38:48 +0800

    把GT6作为前置，然后改了下主类名字

--- src/main/java/Main/MoegAddon.java
+++ src/main/java/Main/MoegAddon.java
@@ -6,19 +6,14 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import item.Flammableice;
 
-@Mod(modid="moegadd", version="1")
-public class Main {
-	@SidedProxy(clientSide = "Main.ClientProxy", 
-    serverSide = "Main.CommonProxy")
+@Mod(modid="moegadd", name="MoegAddon", version="0.1", dependencies="required-after:gregtech")
+public class MoegAddon {
+	@SidedProxy(clientSide = "MoegAddon.ClientProxy", serverSide = "MoegAddon.CommonProxy")
     public static CommonProxy proxy;
+
 	@EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-		
-        proxy.preInit(event);
-    }
+    public void preInit(FMLPreInitializationEvent event) { proxy.preInit(event); }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
--- src/main/java/item/Flammableice.java
+++ src/main/java/item/Flammableice.java
@@ -3,6 +3,7 @@ package item;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import gregapi.data.MT;
 
 public class Flammableice extends Item {{
 
