96dd900dac27d6e4103b2dacd3b3d2c702d6f233
yuesha-yc <kevinwangyichen@gmail.com>
2020-04-07 14:58:06 +0800

    液体,,,,WIP

--- src/main/java/moegaddon/loaders/FluidLoader.java
+++ src/main/java/moegaddon/loaders/FluidLoader.java
@@ -6,22 +6,30 @@ import net.minecraftforge.fluids.Fluid;
 
 public class FluidLoader {
     public static final MoegaddFluid SolutionRichPtPa       = new MoegaddFluid("SolutionRichPtPa");    //富铂钯溶液
-    public static final MoegaddFluid SolutionRichPa         = new MoegaddFluid("SolutionRichPa");    //富钯溶液
-    public static final MoegaddFluid MoltenSodiumBisulfate  = new MoegaddFluid("MoltenSodiumBisulfate");    //熔融硫酸氢钠
-    public static final MoegaddFluid RhodiumSulfideSolution = new MoegaddFluid("RhodiumSulfideSolution");    //硫化铑溶液
-    public static final MoegaddFluid PregnantSolutionOsRu   = new MoegaddFluid("PregnantSolutionOsRu");    //吸收液(富锇钌)
-    public static final MoegaddFluid AbsorptionLiquidRu     = new MoegaddFluid("AbsorptionLiquidRu");    //吸收液（富钌）
-    public static final MoegaddFluid RutheniumTetroxide     = new MoegaddFluid("RutheniumTetroxide");    //四氧化钌
-    public static final MoegaddFluid OsmiumTetroxide        = new MoegaddFluid("OsmiumTetroxide");    //四氧化锇
-    public static final MoegaddFluid ChloroRhodiumAcid      = new MoegaddFluid("ChloroRhodiumAcid");    //氯钌酸
-
     public static final BlockMoegaddFluid BLOCK_SolutionRichPtPa       = new BlockMoegaddFluid(SolutionRichPtPa,"SolutionRichPtPa");    //富铂钯溶液
+
+    public static final MoegaddFluid SolutionRichPa         = new MoegaddFluid("SolutionRichPa");    //富钯溶液
     public static final BlockMoegaddFluid BLOCK_SolutionRichPa         = new BlockMoegaddFluid(SolutionRichPa,"SolutionRichPa");    //富钯溶液
+
+    public static final MoegaddFluid MoltenSodiumBisulfate  = new MoegaddFluid("MoltenSodiumBisulfate");    //熔融硫酸氢钠
     public static final BlockMoegaddFluid BLOCK_MoltenSodiumBisulfate  = new BlockMoegaddFluid(MoltenSodiumBisulfate,"MoltenSodiumBisulfate");    //熔融硫酸氢钠
+
+    public static final MoegaddFluid RhodiumSulfideSolution = new MoegaddFluid("RhodiumSulfideSolution");    //硫化铑溶液
     public static final BlockMoegaddFluid BLOCK_RhodiumSulfideSolution = new BlockMoegaddFluid(RhodiumSulfideSolution,"RhodiumSulfideSolution");    //硫化铑溶液
+
+    public static final MoegaddFluid PregnantSolutionOsRu   = new MoegaddFluid("PregnantSolutionOsRu");    //吸收液(富锇钌)
     public static final BlockMoegaddFluid BLOCK_PregnantSolutionOsRu   = new BlockMoegaddFluid(PregnantSolutionOsRu,"PregnantSolutionOsRu");    //吸收液(富锇钌)
+
+    public static final MoegaddFluid AbsorptionLiquidRu     = new MoegaddFluid("AbsorptionLiquidRu");    //吸收液（富钌）
     public static final BlockMoegaddFluid BLOCK_AbsorptionLiquidRu     = new BlockMoegaddFluid(AbsorptionLiquidRu,"AbsorptionLiquidRu");    //吸收液（富钌）
+
+    public static final MoegaddFluid RutheniumTetroxide     = new MoegaddFluid("RutheniumTetroxide");    //四氧化钌
     public static final BlockMoegaddFluid BLOCK_RutheniumTetroxide     = new BlockMoegaddFluid(RutheniumTetroxide,"RutheniumTetroxide");    //四氧化钌
+
+    public static final MoegaddFluid OsmiumTetroxide        = new MoegaddFluid("OsmiumTetroxide");    //四氧化锇
     public static final BlockMoegaddFluid BLOCK_OsmiumTetroxide        = new BlockMoegaddFluid(OsmiumTetroxide,"OsmiumTetroxide");    //四氧化锇
+
+    public static final MoegaddFluid ChloroRhodiumAcid      = new MoegaddFluid("ChloroRhodiumAcid");    //氯钌酸
     public static final BlockMoegaddFluid BLOCK_ChloroRhodiumAcid      = new BlockMoegaddFluid(ChloroRhodiumAcid,"ChloroRhodiumAcid");    //氯钌酸
+
 }
